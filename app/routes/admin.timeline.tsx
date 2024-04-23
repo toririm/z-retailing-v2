c4d894fa222d7fdfcc82049c63be16c3601c8830
toririm <longtimedragon@icloud.com>
2024-04-24 00:46:32 +0900

    fix: include current month

--- app/routes/admin.timeline.tsx
+++ app/routes/admin.timeline.tsx
@@ -44,10 +44,10 @@ export const loader = async ({ context, request }: LoaderFunctionArgs) => {
 const getMonths = (oldest: Date, newest: Date) => {
 	const dayjs = dayjsJP();
 	const oldestDate = dayjs(oldest).tz().startOf("month");
-	const newestDate = dayjs(newest).tz().startOf("month");
+	const newestDateNextMonth = dayjs(newest).tz().startOf("month").add(1, "month");
 	const months = [];
 	let current = oldestDate;
-	while (current.isBefore(newestDate)) {
+	while (current.isBefore(newestDateNextMonth)) {
 		months.push(current);
 		current = current.add(1, "month");
 	}

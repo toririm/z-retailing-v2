0fbf6fc604509d4578f18a20d32bb05afe9d78ab
toririm <longtimedragon@icloud.com>
2024-05-31 05:50:54 +0900

    feat: 仮名

--- app/routes/timeline.tsx
+++ app/routes/timeline.tsx
@@ -1,5 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { Link, useLoaderData } from "@remix-run/react";
+import { anonUserNames } from "~/.server/anon";
 import { prismaClient } from "~/.server/prisma";
 import { dayjsJP } from "~/utils/dayjs";
 
@@ -25,9 +26,43 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
 					price: true,
 				},
 			},
+			user: {
+				select: {
+					id: true,
+				},
+			},
 		},
 	});
-	return { purchases: purchases.reverse() };
+	const users = purchases
+		.map((purchase) => purchase.user.id)
+		.filter((id, index, array) => array.indexOf(id) === index);
+	const usernames = anonUserNames().slice(0, users.length);
+	type PurchaseWithUsername = {
+		id: string;
+		createdAt: Date;
+		item: {
+			name: string;
+			price: number;
+		};
+		user: {
+			name: string;
+		};
+	};
+	const purchasesWithUsernames: PurchaseWithUsername[] = purchases.map(
+		(purchase) => {
+			const { id, createdAt, item } = purchase;
+			return {
+				id,
+				createdAt,
+				item,
+				user: {
+					name: usernames[users.indexOf(purchase.user.id)],
+				},
+			};
+		},
+	);
+	console.log(purchasesWithUsernames);
+	return { purchases: purchasesWithUsernames.reverse() };
 };
 
 export default function Timeline() {
@@ -51,16 +86,18 @@ export default function Timeline() {
 				<table className="table table-zebra">
 					<thead className="sticky top-0 bg-base-100">
 						<tr>
+							<th />
 							<th>日時</th>
 							<th>内容</th>
 						</tr>
 					</thead>
 					<tbody>
-						{purchases.map((purchase) => (
+						{purchases.map((purchase, index) => (
 							<tr key={purchase.id}>
+								<th>{purchases.length - index}</th>
 								<td>{dayjs(purchase.createdAt).tz().format("M/D H:mm")}</td>
 								<td>
-									誰かが
+									{purchase.user.name}が
 									<span className="font-bold pl-1">{purchase.item.name}</span>
 									<span className="pr-1">（&yen; {purchase.item.price}）</span>
 									を購入したよ！
